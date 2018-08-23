3a26eb8099e9a9110a10211ba0301e65998dc10d
Zaine Kingi <rous6026nz@gmail.com>
2018-08-24 10:53:58 +1200

    Export new user function.

--- db.js
+++ db.js
@@ -4,7 +4,8 @@ const connection = require('knex')(config)
 
 module.exports = {
   getUser: getUser,
-  getUsers: getUsers
+  getUsers: getUsers,
+  newUser: newUser
 }
 
 function getUsers (testConn) {
