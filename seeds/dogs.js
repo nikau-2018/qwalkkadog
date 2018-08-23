ef609d6856a2dc761c34d302d0ab412c3689aacd
Tommy Hall <thall2906@gmail.com>
2018-08-24 11:06:13 +1200

    Done

--- seeds/dogs.js
+++ seeds/dogs.js
@@ -2,7 +2,7 @@
 exports.seed = (knex, Promise) => {
   // Deletes ALL existing entries
   return knex('dogs').del()
-    .then( () => {
+    .then(() => {
       // Inserts seed entries
       return knex('dogs').insert([
         {id: 33301, name: 'Rocco', size: 'Large', location: 'Otara', gender: 'Male', breed: 'Chihuahua', bio: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.', profile_pic: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQK9XcFIFqxYuVu_H9Hgv5yVTRIsv_arT9teAZ4-LN5ghb0DcpE'},
@@ -11,4 +11,3 @@ exports.seed = (knex, Promise) => {
       ])
     })
 }
-
